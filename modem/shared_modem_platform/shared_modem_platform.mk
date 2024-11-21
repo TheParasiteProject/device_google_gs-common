74283c5cbe4c40bf5765745795424cd46454eacc
Liana Kazanova (xWF) <lkazanova@google.com>
2024-11-21 17:53:56 +0000

    Revert "modem_svc: move shared_modem_platform related sepolicy t..."
    Revert submission 30519089-move_modem_sepolicy
    
    Reason for revert: DroidMonitor: Potential culprit for http://b/380274930 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Reverted changes: /q/submissionid:30519089-move_modem_sepolicy
    
    Change-Id: I241b3aba370f77c705ca3890151e760b4764beca

--- modem/shared_modem_platform/shared_modem_platform.mk
+++ modem/shared_modem_platform/shared_modem_platform.mk
@@ -8,4 +8,4 @@ SOONG_CONFIG_shared_modem_platform_vendor := $(SHARED_MODEM_PLATFORM_VENDOR)
 
 PRODUCT_PACKAGES += shared_modem_platform
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/modem/shared_modem_platform/compatibility_matrix.xml
-BOARD_SEPOLICY_DIRS += device/google/gs-common/modem/shared_modem_platform/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/modem/shared_modem_platform/sepolicy
