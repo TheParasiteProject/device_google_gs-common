8112ee64cc93db0c674bec442b2419e9ea6249a4
Eileen Lai <eileenisgood@google.com>
2024-12-06 12:49:05 +0000

    modem_svc: add modem_svc_sit to solve sepolicy conflicts arising from different device versions
    Add modem_svc_sit to gs-common. modem_svc_sit will support devices from gs101 to zuma_pro. The shared_modem_platform will be updated to support devices after zumapro (not include).
    
    Bug: 372400955
    
    Flag: NONE local testing only
    Change-Id: I0bcc9baf69045327e6fcca92ab8003e303db2649

--- modem/shared_modem_platform/shared_modem_platform.mk
+++ modem/shared_modem_platform/shared_modem_platform.mk
@@ -1,6 +1,8 @@
 # This file is not included in `modem.mk` since this is included at the
 # beginning of each `device.mk` file, and so will be called before
 # `SHARED_MODEM_PLATFORM_VENDOR` is defined later in the file.
+# This file supports support devices which AP is after zumapro(not include).
+# For device before zumapro(include) please use gs-common/modem/modem_svc_sit/shared_modem_platform.mk
 SOONG_CONFIG_NAMESPACES += shared_modem_platform
 SOONG_CONFIG_shared_modem_platform += \
        vendor
